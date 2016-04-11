17ce12e6bbfa1d3d5ef38b904fa086ca794efa0a
Tonye Brown <tonye96@gmail.com>
2016-04-11 00:15:04 -0400

    Update convolution.sv

--- convolution.sv
+++ convolution.sv
@@ -1,28 +1,24 @@
 module convolution( 
 input logic clk,
-input reg[1023:0][15:0] pixels_in,
-input reg[8:0][15:0] mask,
+input reg[35:0][15:0] pixels_in,
+input reg [8:0] [15:0] mask,
 input logic[15:0] bias,
-output logic[899:0][15:0] pixels_out);
+output logic[15:0][15:0] pixels_out);
+ 
+logic [15:0][15:0] pix_out;
 
-logic [899:0][15:0] pix_out;
+genvar i,j;
 
-genvar i, j;
-generate for (i = 1; i < 31; i++) begin: for_i
-    generate for (j = 1; j < 31; j++) begin: for_j
-        wire[15:0] index;
-	assign index = i + j*32; 
-
-	conv con(.clk (clk),
-	    .in  ({pixels_in[(index-33):(index-31)],
-		   pixels_in[(index-1):(index+1)],
-		   pixels_in[(index+31):(index+33)]}),
-	    .kernel (mask),
-	    .out (pix_out[index]));
-	assign pixels_out[index] = pix_out[index] + bias;
-
-    end
-    endgenerate
+generate for (i = 1; i < 5; i++) begin: for_i
+		for (j = 1; j < 5; j++) begin: for_j
+		conv con(.clk (clk),
+				.in  ({pixels_in[((i + (j*6))-5):((i + (j*6))-7)], 
+							pixels_in[((i + (j*6))+1):((i + (j*6))-1)],
+							pixels_in[((i + (j*6))+7):((i + (j*6))+5)]}),
+				.kernel (mask),
+				.out (pix_out[((i-1)*4) + (j-1)]));
+		assign pixels_out[((i-1)*4) + (j-1)] = pix_out[((i-1)*4) + (j-1)] - bias;
+		end
 end
 endgenerate
 

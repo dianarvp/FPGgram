1e99072203761778c85d82991016ca1b403fca17
Tonye Brown <tonye96@gmail.com>
2016-04-11 23:58:01 -0400

    Update NET_Controller.sv

--- NET_Controller.sv
+++ NET_Controller.sv
@@ -1,14 +1,28 @@
 module NET_Controller(input logic clk,
-							 input logic reset,
-							input logic [15:0] pixel,
-							input logic [31:0] address,
-							input logic [15:0] instruction,
-							output logic ready);
+			input logic reset,
+			input logic [15:0] pixel,
+			input logic [31:0] address,
+			input logic [15:0] instruction,
+			output logic ready);
 
+reg[35:0][15:0] pixel_conv;
+reg[1023:0][15:0] pixel_avg;
+
+convolution conv_unit();
+avg_pool pool_unit();
 
 
 always_ff @(posedge clk) begin
 	if (reset) begin
 	end 
-	else 
-		
+	else begin
+		case (instruction[3:1])
+			2'b001: begin //convolution
+			end
+			2'b010: begin //avg_pool
+			end
+			2'b100: begin //backprop
+			end
+	end
+end
+endmodule
